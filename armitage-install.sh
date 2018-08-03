64e6e04dbb435a622ea3d766a8aa3c50f7c3a045
profionaldhim <35074237+profionaldhim@users.noreply.github.com>
2018-08-03 17:52:11 -0400

    Add files via upload

--- armitage-install.sh
+++ armitage-install.sh
@@ -2,9 +2,6 @@
 apt install unzip -y
 apt install postgresql -y
 
-git clone https://github.com/profionaldhim/M.Info-Kali-Nethunter
-
-cd M.Info-Kali-Nethunter
 
 mv M.Info-Kali-Nethunter.zip $HOME/kali-armhf/root
 
